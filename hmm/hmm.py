ce4dec9a3a5d13934853c0b7b33a2bd274b41993
Nick Bouonatsos <nikosbu@hotmail.com>
2022-11-29 19:39:02 +0100

    Fixed forward

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -75,15 +75,18 @@ def forward(X,A,E):
     #####################
     # START CODING HERE #
     #####################
-    # HINT: The Viterbi and Forward algorithm are very similar! 
+    # HINT: The Viterbi and Forward algorithm are very similar!
     # Adapt the viterbi() function to account for the differences.
 
     # Middle columns
-    # for ...
+    for i,s in enumerate(X):
+        for l in emittingStates:
+            terms = [F[k][i] * A[k][l] for k in allStates]
+            F[l][i+1] = sum(terms) * E[l][s]
 
     # Last columns
-    # for ...:
-    #     F['E'][-1] += ...
+    for k in allStates:
+        F['E'][-1] += A[k]['E'] * F[k][i+1]
 
     #####################
     #  END CODING HERE  #
---
+++ hmm/run_viterbi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.10
+# -*- coding: utf-8 -*-
+
+import os
+
+def main():
+	os.system("/home/banoffee/Documents/asa-project/hmm/hmm.py -vv viterbi /home/banoffee/Documents/asa-project/hmm/input/sequence_X1.fasta /home/banoffee/Documents/asa-project/hmm/input/A1_prior.tsv /home/banoffee/Documents/asa-project/hmm/input/E1_prior.tsv")
+
+
+if __name__ == "__main__":
+    main()
