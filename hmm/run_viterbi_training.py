6630534279ca8002f1c40705560c1051aa99f304
Nick Bouonatsos <nikosbu@hotmail.com>
2022-12-13 12:09:53 +0100

    Added results matrixes

--- hmm/run_viterbi_training.py
+++ hmm/run_viterbi_training.py
@@ -4,7 +4,7 @@
 import os
 
 def main():
-	os.system("/home/banoffee/Documents/asa-project/hmm/hmm.py -vv baumwelch /home/banoffee/Documents/asa-project/hmm/input/training_sequences.fasta /home/banoffee/Documents/asa-project/hmm/input/A1_prior.tsv /home/banoffee/Documents/asa-project/hmm/input/E1_prior.tsv")
+	os.system("/home/banoffee/Documents/asa-project/hmm/hmm.py -vv baumwelch /home/banoffee/Documents/asa-project/hmm/input/training_sequences.fasta /home/banoffee/Documents/asa-project/hmm/input/A2.tsv /home/banoffee/Documents/asa-project/hmm/input/E4.tsv")
 
 
 if __name__ == "__main__":
